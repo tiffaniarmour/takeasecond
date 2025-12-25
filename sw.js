27da05430e51069c6e1b996404d16f91a23daa9e
Tiffani Armour <71851531+tiffaniarmour@users.noreply.github.com>
2025-12-25 13:54:38 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,31 @@
-const CACHE = "take-a-second-v1";
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll([
-      "./","./index.html","./styles.css","./app.js","./manifest.json"
-    ]))
+// sw.js
+const CACHE_NAME = "digital-bloom-regulation-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
-self.addEventListener("fetch",e=>{
-  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k === CACHE_NAME ? null : caches.delete(k))))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request))
+  );
 });
